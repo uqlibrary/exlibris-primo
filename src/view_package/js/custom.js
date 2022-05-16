de2f193d134601939ce1324c5b81bb4569062b2f
Lea de Groot <l.degroot@uq.edu.au>
2022-05-17 08:56:02 +1000

    supply the ITS parent GTM id that will dynamically insert our local id [PT #182123345]

--- src/view_package/js/custom.js
+++ src/view_package/js/custom.js
@@ -6,7 +6,7 @@
   app.component('prmTopBarBefore', {
     // we found it was more robust to insert the askus button in the different page location via primo angular, see below,
     // so completely skip inserting elements "by attribute"
-    template: '<uq-gtm gtm="GTM-W4KK37"></uq-gtm>' +
+    template: '<uq-gtm gtm="GTM-NC7M38Q"></uq-gtm>' +
         '<uq-header hideLibraryMenuItem="true" searchLabel="library.uq.edu.au" searchURL="http://library.uq.edu.au" skipnavid="searchBar"></uq-header>' +
         '<uq-site-header hideMyLibrary hideAskUs></uq-site-header>'
   });
