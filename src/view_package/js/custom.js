51175a426657ced6709282465196f0b651f80bc0
Lea de Groot <l.degroot@uq.edu.au>
2022-07-28 09:47:02 +1000

    WIP: add mylibrary options to the account button [PT #182204834]

--- src/view_package/js/custom.js
+++ src/view_package/js/custom.js
@@ -170,4 +170,105 @@
   insertStylesheet('https://static.uq.net.au/v6/fonts/Roboto/roboto.css');
   insertStylesheet('https://static.uq.net.au/v9/fonts/Merriweather/merriweather.css');
   insertStylesheet('https://static.uq.net.au/v13/fonts/Montserrat/montserrat.css');
+
+  var listMyLibraryLinks = [
+    {
+      title: 'Borrowing',
+      link: 'https://search.library.uq.edu.au/primo-explore/login?vid=61UQ&targetURL=https%3A%2F%2Fsearch.library.uq.edu.au%2Fprimo-explore%2Faccount%3Fvid%3D61UQ%26lang%3Den_US%26section%3Dloans',
+      svg: 'M17.5 4.5c-1.95 0-4.05.4-5.5 1.5-1.45-1.1-3.55-1.5-5.5-1.5S2.45 4.9 1 6v14.65c0 .65.73.45.75.45C3.1 20.45 5.05 20 6.5 20c1.95 0 4.05.4 5.5 1.5 1.35-.85 3.8-1.5 5.5-1.5 1.65 0 3.35.3 4.75 1.05.41.21.75-.19.75-.45V6c-1.49-1.12-3.63-1.5-5.5-1.5zm3.5 14c-1.1-.35-2.3-.5-3.5-.5-1.7 0-4.15.65-5.5 1.5V8c1.35-.85 3.8-1.5 5.5-1.5 1.2 0 2.4.15 3.5.5v11.5z',
+    },
+    {
+      title: 'Document delivery',
+      link: 'https://auth.library.uq.edu.au/login?relais_return=1',
+      svg: 'M17.5 4.5c-1.95 0-4.05.4-5.5 1.5-1.45-1.1-3.55-1.5-5.5-1.5S2.45 4.9 1 6v14.65c0 .65.73.45.75.45C3.1 20.45 5.05 20 6.5 20c1.95 0 4.05.4 5.5 1.5 1.35-.85 3.8-1.5 5.5-1.5 1.65 0 3.35.3 4.75 1.05.41.21.75-.19.75-.45V6c-1.49-1.12-3.63-1.5-5.5-1.5zm3.5 14c-1.1-.35-2.3-.5-3.5-.5-1.7 0-4.15.65-5.5 1.5V8c1.35-.85 3.8-1.5 5.5-1.5 1.2 0 2.4.15 3.5.5v11.5z',
+    },
+    {
+      title: 'Learning resources',
+      link: 'https://www.library.uq.edu.au/learning-resources',
+      svg: 'M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z',
+    },
+    {
+      title: 'Print balance',
+      link: 'https://lib-print.library.uq.edu.au:9192/user',
+      svg: 'M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z',
+    },
+    // {
+    //   title: 'eSpace dashboard',
+    //   link: 'https://espace.library.uq.edu.au/dashboard',
+    //   svg: 'M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z',
+    // },
+    {
+      title: 'Room bookings',
+      link: 'https://uqbookit.uq.edu.au/#/app/booking-types/77b52dde-d704-4b6d-917e-e820f7df07cb',
+      svg: 'M2 17h20v2H2zm11.84-9.21c.1-.24.16-.51.16-.79 0-1.1-.9-2-2-2s-2 .9-2 2c0 .28.06.55.16.79C6.25 8.6 3.27 11.93 3 16h18c-.27-4.07-3.25-7.4-7.16-8.21z',
+    },
+    // {
+    //   title: 'Saved items',
+    //   link: 'https://search.library.uq.edu.au/primo-explore/login?vid=61UQ&targetURL=https%3A%2F%2Fsearch.library.uq.edu.au%2Fprimo-explore%2Ffavorites%3Fvid%3D61UQ%26lang%3Den_US%26section%3Ditems',
+    //   svg: 'M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z',
+    // },
+    {
+      title: 'Saved searches',
+      link: 'https://search.library.uq.edu.au/primo-explore/login?vid=61UQ&targetURL=https%3A%2F%2Fsearch.library.uq.edu.au%2Fprimo-explore%2Ffavorites%3Fvid%3D61UQ%26lang%3Den_US%26section%3Dqueries',
+      svg: 'M17.01 14h-.8l-.27-.27c.98-1.14 1.57-2.61 1.57-4.23 0-3.59-2.91-6.5-6.5-6.5s-6.5 3-6.5 6.5H2l3.84 4 4.16-4H6.51C6.51 7 8.53 5 11.01 5s4.5 2.01 4.5 4.5c0 2.48-2.02 4.5-4.5 4.5-.65 0-1.26-.14-1.82-.38L7.71 15.1c.97.57 2.09.9 3.3.9 1.61 0 3.08-.59 4.22-1.57l.27.27v.79l5.01 4.99L22 19l-4.99-5z',
+    },
+    {
+      title: 'Feedback',
+      link: 'https://support.my.uq.edu.au/app/library/feedback',
+      svg: 'M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7 12h-2v-2h2v2zm0-4h-2V6h2v4z',
+    }
+  ];
+
+  // get elem md-menu-content.prm-user-menu-content
+  var parentElem = document.querySelector('md-menu-content.prm-user-menu-content');
+  // var parentElem = document.querySelector('#skiptohere');
+  if (!!parentElem) {
+    listMyLibraryLinks.forEach((e, i) => {
+      var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      // var path = document.createElement('path');
+      !!path && (path.setAttribute('d', e.svg));
+
+      var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+      // var svg = document.createElement('svg');
+      !!svg && (svg.width = '100%');
+      !!svg && (svg.height = '100%');
+      !!svg && svg.setAttribute('viewBox', '0 0 24 24');
+      // !!svg && svg.setAttribute('y', '0');
+      // !!svg && svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+      // !!svg && (svg.fit = '');
+      // !!svg && svg.setAttribute('preserveAspectRatio', 'xMidYMid meet');
+      !!svg && svg.setAttribute('focusable', 'false');
+      !!svg && !!path && svg.appendChild(path);
+
+      // var mdIcon = document.createElement('span');
+      // !!mdIcon && (mdIcon.role = 'presentation');
+      // !!mdIcon && (mdIcon.className = 'md-primoExplore-theme');
+      // !!mdIcon && !!svg && mdIcon.appendChild(svg);
+
+      // var prmIcon = document.createElement('span');
+      // !!prmIcon && (prmIcon.className = 'rotate-25');
+      // !!prmIcon && prmIcon.setAttribute('icon-type', 'svg');
+      // !!prmIcon && prmIcon.setAttribute('svg-icon-set', 'primo-ui');
+      // !!prmIcon && prmIcon.setAttribute('icon-definition', 'prm_pin');
+      // !!prmIcon && !!mdIcon && prmIcon.appendChild(mdIcon);
+
+      var button = document.createElement('button');
+      !!button && (button.className = 'mylibrary button-with-icon md-button md-primoExplore-theme md-ink-ripple');
+      !!button && (button.type = 'button');
+      !!button && button.setAttribute('aria-label', `Go to ${e.title}`);
+      !!button && (button.role = 'menuitem');
+      !!button && (button.href = e.link);
+      // !!button && !!prmIcon && button.appendChild(prmIcon);
+      !!button && !!svg && button.appendChild(svg);
+
+      var text = document.createTextNode(e.title);
+      var span = document.createElement('span');
+      !!span && !!text && span.appendChild(text);
+      !!button && !!span && button.appendChild(span);
+
+      var mdMenuItem = document.createElement('div');
+      !!mdMenuItem && !!button && mdMenuItem.appendChild(button);
+      !!mdMenuItem && parentElem.appendChild(mdMenuItem);
+    })
+  }
 })();
