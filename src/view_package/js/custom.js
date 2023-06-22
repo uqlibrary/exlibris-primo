85540c32763b7807d0fdb9a4e95ac1387489296d
Lea de Groot <l.degroot@uq.edu.au>
2023-06-23 09:38:05 +1000

    change account menu item from papercut link to how-to drupal link [PT #185449236]
    they need a campus IP for that link to work, so better to send them to a help page

--- src/view_package/js/custom.js
+++ src/view_package/js/custom.js
@@ -132,13 +132,13 @@ function whenPageLoaded(fn) {
 			"    <li>\n" +
 			'        <button class="button-with-icon md-primoExplore-theme md-ink-ripple" type="button"' +
 			'				data-analyticsid="mylibrary-menu-print-balance" aria-label="Go to Print balance" role="menuitem"' +
-			'				onclick="javascript:window.open(\'https://lib-print.library.uq.edu.au:9192/user\', \'_blank\');">\n' +
+			'				onclick="javascript:window.open(\'https://web.library.uq.edu.au/library-services/it/print-scan-copy/your-printing-account\', \'_blank\');">\n' +
 			'            <svg viewBox="0 0 24 24" focusable="false">\n' +
 			'                <path d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z"></path>\n' +
 			"            </svg>\n" +
 			'            <div class="textwrapper">\n' +
 			'                <span class="primaryText">Print balance</span>\n' +
-			'                <span class="subtext">Log in to your print account</span>\n' +
+			'                <span class="subtext">How to check your balance and top up</span>\n' +
 			"            </div>\n" +
 			"        </button>\n" +
 			"    </li>\n" +
