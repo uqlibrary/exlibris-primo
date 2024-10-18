01039e374bd0a8e874d48b9a630a51d4c991f17d
Lea de Groot <l.degroot@library.uq.edu.au>
2024-10-18 11:39:51 +1000

    move the log out button to the bottom [PT #188445123]

--- src/view_package/js/custom.js
+++ src/view_package/js/custom.js
@@ -345,7 +345,7 @@ function whenPageLoaded(fn) {
 					}
 				} else if (isDesktopMenuOpen) {
 					const createdMenuItem = document.getElementById(DESKTOP_LOGGED_IN_FEEDBACK_ID);
-					if (!createdMenuItem) {
+					if (!createdMenuItem && !!desiredParentDesktop) {
 
 						// append new Account links to existing menu
 						// note: we use the native favourites on desktop
@@ -353,6 +353,10 @@ function whenPageLoaded(fn) {
 						desiredParentDesktop.insertAdjacentHTML('beforeend', ourPrintBalanceMenuItem());
 						desiredParentDesktop.insertAdjacentHTML('beforeend', ourRoomBookingMenuItem());
 						desiredParentDesktop.insertAdjacentHTML('beforeend', ourFeedbackMenuItem(DESKTOP_LOGGED_IN_FEEDBACK_ID));
+
+						// move the logout button to the bottom
+						const logoutButton = document.querySelector('md-menu-item .user-menu-header button');
+						!!logoutButton && desiredParentDesktop.appendChild(logoutButton);
 
 						// delete the items they provide because we have similar in our account links list
 						const deletionClassList = [
