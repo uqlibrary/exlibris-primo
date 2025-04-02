0de20fd71dad5d4406ba07858e67f7adb3c547c9
Lea de Groot <l.degroot@library.uq.edu.au>
2025-04-03 09:36:51 +1000

    fix menus [PT #188391821]

--- src/VIEW_CODE/js/custom.js
+++ src/VIEW_CODE/js/custom.js
@@ -82,41 +82,45 @@ function whenPageLoaded(fn) {
 	const accountLinkOptions = {
 		title: "Library account",
 		id: "mylibrary-menu-borrowing",
-		svgString: '<svg viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">' +
+		svgString: '<svg data-testid="library-account-icon" id="library-account-icon" viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">' +
 			'<path d="M12 3C14.2222 3 16 4.77778 16 7C16 9.22222 14.2222 11 12 11C9.77778 11 8 9.22222 8 7C8 4.77778 9.77778 3 12 3Z" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"/>' +
 			'<path d="M4.59961 20.5716C4.59961 16.4685 7.91578 13.1523 12.0188 13.1523C16.1219 13.1523 19.438 16.4685 19.438 20.5716" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"/>' +
-			'</svg>'
+			'</svg>',
+		svgId: 'library-account-icon',
 	};
 	const favouriteLinkOptions = {
 		title: "Favourites",
 		id: "mylibrary-menu-saved-items",
-		svgString: '<svg viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">' +
+		svgString: '<svg data-testid="library-favourites-icon" id="library-favourites-icon" viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">' +
 			'<path d="M12.6988 3.43449L14.9056 7.8896C14.9557 8.00269 15.0347 8.10063 15.1345 8.17369C15.2344 8.24675 15.3516 8.29235 15.4746 8.30597L20.3461 9.02767C20.4871 9.04581 20.6201 9.1037 20.7294 9.19458C20.8388 9.28545 20.9201 9.40559 20.9639 9.54094C21.0074 9.67627 21.0117 9.82125 20.9761 9.95891C20.9404 10.0966 20.8663 10.2213 20.7625 10.3184L17.2511 13.802C17.1615 13.8857 17.0942 13.9905 17.0554 14.1069C17.0167 14.2232 17.0075 14.3474 17.0291 14.4682L17.8757 19.3674C17.9001 19.5081 17.8847 19.653 17.831 19.7854C17.7771 19.9178 17.6873 20.0325 17.5717 20.1163C17.456 20.2003 17.3191 20.25 17.1765 20.2598C17.0339 20.2698 16.8915 20.2394 16.7654 20.1724L12.3796 17.8546C12.2673 17.7995 12.1439 17.7708 12.0188 17.7708C11.8936 17.7708 11.7702 17.7995 11.6579 17.8546L7.27219 20.1724C7.14601 20.2394 7.00355 20.2698 6.861 20.2598C6.71845 20.25 6.58153 20.2003 6.46585 20.1163C6.35016 20.0325 6.26034 19.9178 6.2066 19.7854C6.15286 19.653 6.13737 19.5081 6.16188 19.3674L7.00849 14.4127C7.02995 14.2919 7.02087 14.1677 6.98209 14.0514C6.9433 13.935 6.87604 13.8302 6.78643 13.7465L3.23344 10.3184C3.12833 10.2186 3.05441 10.0905 3.02063 9.94953C2.98686 9.80858 2.99468 9.66085 3.04315 9.52425C3.09162 9.38766 3.17866 9.26805 3.29372 9.17991C3.40879 9.09178 3.54694 9.0389 3.69145 9.02767L8.56292 8.30597C8.68589 8.29235 8.80314 8.24675 8.90298 8.17369C9.00281 8.10063 9.08177 8.00269 9.13195 7.8896L11.3387 3.43449C11.3988 3.30474 11.4947 3.19489 11.6153 3.1179C11.7358 3.04091 11.8758 3 12.0188 3C12.1617 3 12.3018 3.04091 12.4223 3.1179C12.5428 3.19489 12.6387 3.30474 12.6988 3.43449Z" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"/>' +
 			'</svg>',
 		link: `/discovery/favorites?vid=${vidParam}&lang=en&section=items`,
-	};
+		svgId: 'library-favourites-icon',
+	}; // searchHistoryOptions
 	const searchHistoryOptions = {
 		title: "Search history",
 		id: "mylibrary-menu-saved-items",
-		svgString: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">' +
+		svgString: '<svg data-testid="library-search-history-icon" id="library-search-history-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">' +
 			'<path d="M6.78771 6.69579C8.04789 5.28681 9.76183 4.36381 11.6318 4.08712C13.5017 3.81043 15.4095 4.19754 17.0237 5.1812C18.6379 6.16487 19.8566 7.68292 20.4679 9.47165C21.0792 11.2604 21.0446 13.2067 20.3701 14.9726C19.6956 16.7385 18.4238 18.2123 16.7757 19.138C15.1275 20.0636 13.2072 20.3827 11.3482 20.0397C9.48931 19.6968 7.80926 18.7135 6.59994 17.2606C5.39063 15.8077 4.72848 13.9771 4.72852 12.0868V11.2356" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"/>' +
 			'<path d="M6.8564 13.7891L4.72825 11.2354L2.6001 13.7891" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"/>' +
 			'<path d="M12.8154 8.25586V12.5122H15.7948" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"/>' +
 			'</svg>',
 		link: `/discovery/favorites?vid=${vidParam}&amp;lang=en&amp;section=search_history`,
+		svgId: 'library-search-history-icon',
 	};
 	const feedbackOptions = {
 		title: "Feedback",
 		ariaLabel: "Provide feedback",
 		link: "https://support.my.uq.edu.au/app/library/feedback",
 		id: "mylibrary-menu-feedback",
-		svgString: '<svg viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">' +
+		svgString: '<svg data-testid="library-feedback-icon" id="library-feedback-icon" viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">' +
 			'<path d="M19.7998 17.3998H11.3999L6.59995 20.9998V17.3998H4.19998C3.88173 17.3998 3.57651 17.2734 3.35147 17.0483C3.12643 16.8233 3 16.5181 3 16.1998V4.19998C3 3.88173 3.12643 3.57651 3.35147 3.35147C3.57651 3.12643 3.88173 3 4.19998 3H19.7998C20.118 3 20.4233 3.12643 20.6483 3.35147C20.8733 3.57651 20.9998 3.88173 20.9998 4.19998V16.1998C20.9998 16.5181 20.8733 16.8233 20.6483 17.0483C20.4233 17.2734 20.118 17.3998 19.7998 17.3998Z" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"/>' +
 			'<path d="M6.59961 8.39941H17.3995" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"/>' +
 			'<path d="M6.59961 12H14.9995" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"/>' +
 			'</svg>',
 		newWindow: true,
 		className: "my-feedback-ctm",
+		svgId: 'library-feedback-icon',
 	};
 
 	function ourFeedbackMenuItem(feedbackId) {
@@ -152,7 +156,7 @@ function whenPageLoaded(fn) {
 			'    <button class="button-with-icon md-primoExplore-theme md-ink-ripple" type="button"' +
 			'			data-analyticsid="mylibrary-menu-course-resources" aria-label="Go to Learning resources" role="menuitem"' +
 			'			onclick="javascript:window.open(\'https://www.library.uq.edu.au/learning-resources\', \'_blank\');">\n' +
-			'<svg viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">' +
+			'<svg data-testid="library-learning-resources-icon" viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">' +
 			'<path d="M11.9999 21.4003V6.78587C11.9999 6.78587 9.94278 4.51443 2.99986 4.42871C2.87129 4.42871 2.78558 4.47157 2.69986 4.55728C2.61415 4.643 2.57129 4.72871 2.57129 4.85729V18.5717C2.57129 18.786 2.74272 19.0003 2.99986 19.0003C9.94278 19.1288 11.9999 21.4003 11.9999 21.4003Z" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"></path>' +
 			'<path d="M9.46999 12.2291C8.05569 11.7577 6.55568 11.4577 5.05566 11.3291" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"></path>' +
 			'<path d="M9.46999 15.7428C8.05569 15.2713 6.55568 14.9713 5.05566 14.8428" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"></path>' +
@@ -168,12 +172,12 @@ function whenPageLoaded(fn) {
 	}
 
 	function ourPrintBalanceMenuItem() {
-		const ICON_SVG_PRINTER = 'M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z';
+		// const ICON_SVG_PRINTER = 'M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z';
 		return '<md-menu-item data-testid="ourPrintBalanceMenuItem" class="uql-account-menu-option">\n' +
 			'    <button class="button-with-icon md-primoExplore-theme md-ink-ripple" type="button"' +
 			'			data-analyticsid="mylibrary-menu-print-balance" aria-label="Go to Print balance" role="menuitem"' +
 			'			onclick="javascript:window.open(\'https://web.library.uq.edu.au/library-and-student-it-help/print-scan-and-copy/your-printing-account\', \'_blank\');">\n' +
-			'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">' +
+			'<svg data-testid="library-print-balance-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">' +
 			'<g clip-path="url(#clip0_1723_14098)">' +
 			'<path d="M3.01562 12C3.01563 14.3828 3.96219 16.668 5.64709 18.3529C7.33198 20.0378 9.6172 20.9844 12 20.9844C14.3828 20.9844 16.668 20.0378 18.3529 18.3529C20.0378 16.668 20.9844 14.3828 20.9844 12C20.9844 9.6172 20.0378 7.33198 18.3529 5.64709C16.668 3.96219 14.3828 3.01563 12 3.01562C9.6172 3.01563 7.33198 3.96219 5.64709 5.64709C3.96219 7.33198 3.01563 9.6172 3.01562 12Z" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"/>' +
 			'<path d="M10.2031 13.7969C10.2031 14.1523 10.3085 14.4997 10.506 14.7952C10.7034 15.0907 10.984 15.321 11.3124 15.457C11.6407 15.593 12.002 15.6286 12.3506 15.5592C12.6991 15.4899 13.0193 15.3188 13.2706 15.0675C13.5219 14.8162 13.693 14.496 13.7623 14.1474C13.8317 13.7989 13.7961 13.4376 13.6601 13.1092C13.5241 12.7809 13.2938 12.5003 12.9983 12.3028C12.7028 12.1054 12.3554 12 12 12C11.6446 12 11.2972 11.8946 11.0017 11.6972C10.7062 11.4997 10.4759 11.2191 10.3399 10.8908C10.2039 10.5624 10.1683 10.2011 10.2377 9.85257C10.307 9.50401 10.4781 9.18384 10.7294 8.93254C10.9807 8.68125 11.3009 8.51011 11.6494 8.44078C11.998 8.37144 12.3593 8.40703 12.6876 8.54303C13.016 8.67903 13.2966 8.90934 13.494 9.20484C13.6915 9.50033 13.7969 9.84774 13.7969 10.2031" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"/>' +
@@ -199,7 +203,7 @@ function whenPageLoaded(fn) {
 			'    <button class="button-with-icon md-primoExplore-theme md-ink-ripple" type="button"' +
 			'			data-analyticsid="mylibrary-menu-room-bookings" aria-label="Go to Book a room or desk" role="menuitem"' +
 			'			onclick="javascript:window.open(\'https://uqbookit.uq.edu.au/#/app/booking-types/77b52dde-d704-4b6d-917e-e820f7df07cb\', \'_blank\');">\n' +
-			'<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">' +
+			'<svg data-testid="library-room-booking-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">' +
 			'<path d="M2.18907 3.41406H17.8109C18.467 3.41406 19 3.94588 19 4.60043V17.8141C19 18.4686 18.467 19.0004 17.8109 19.0004H2.18907C1.53303 19.0004 1 18.4686 1 17.8141V4.60043C1 3.94588 1.53303 3.41406 2.18907 3.41406Z" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"></path>' +
 			'<path d="M1 8.2002H18.5399" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"></path>' +
 			'<path d="M5.79688 5.21364V1" stroke="#51247A" stroke-linecap="round" stroke-linejoin="round"></path>' +
@@ -216,6 +220,12 @@ function whenPageLoaded(fn) {
 	function rewriteProvidedPrimoButton(buttonOptions, primoIdentifier) {
 		const button = document.querySelector(primoIdentifier + " button");
 		if (!button) {
+			return;
+		}
+
+		const svgExists = document.querySelector(primoIdentifier + ' button svg#' + buttonOptions.svgId)
+		if (!!svgExists) {
+			// we've already created the replacement
 			return;
 		}
 
@@ -250,6 +260,8 @@ function whenPageLoaded(fn) {
 
 				// add an ID for GTM usage to the button
 				const menuItem = document.querySelector(primoIdentifier + ' button');
+				console.log('menuItem ', buttonOptions.id, menuItem);
+				!!menuItem && !!button && menuItem.appendChild(button);
 				!!menuItem && menuItem.setAttribute('data-analyticsid', buttonOptions.id);
 				!!menuItem && menuItem.setAttribute('data-testid', buttonOptions.id);
 			}
@@ -402,11 +414,11 @@ function whenPageLoaded(fn) {
 							!!elem && elem.remove();
 						});
 
-						rewriteProvidedPrimoButton(accountLinkOptions, '.my-library-card-ctm');
+						rewriteProvidedPrimoButton(accountLinkOptions, '.my-library-card-ctm', 'library-account-icon');
 
-						rewriteProvidedPrimoButton(favouriteLinkOptions, '.my-favorties-ctm');
+						rewriteProvidedPrimoButton(favouriteLinkOptions, '.my-favorties-ctm', 'library-favourites-icon');
 
-						// remove the dividers, having removed all the contents of the block (TODO change to querySelectorAll)
+						// remove the dividers, having removed all the contents of the block
 						const hr1 = document.querySelector("md-menu-divider");
 						!!hr1 && hr1.remove();
 						const hr2 = document.querySelector("md-menu-divider");
@@ -439,8 +451,7 @@ function whenPageLoaded(fn) {
 		}, timeout);
 	}
 
-	// prm-explore-main-after
-	app.component("prmExploreMainAfter", {
+	app.component("prmExploreFooterAfter", { // prm-explore-footer-after
 		// HANDLE LOGGED OUT VIEW
 		controller: function ($scope) {
 			const awaitLoggedout = setInterval(() => {
@@ -459,8 +470,8 @@ function whenPageLoaded(fn) {
 						const desiredParentMobile = !!mobilesibling && mobilesibling.parentNode;
 
 						const feedbackId = MOBILE_LOGGED_OUT_FEEDBACK_ID; // unique to mobile logged out
-						const createdMenuItem = document.getElementById(feedbackId);
-						if (!!desiredParentMobile && !createdMenuItem) {
+						const createdFeedbackMenuItem = document.getElementById(feedbackId);
+						if (!!desiredParentMobile && !createdFeedbackMenuItem) {
 							desiredParentMobile.insertAdjacentHTML('beforeend', ourFeedbackMenuItem(feedbackId));
 
 							const feedbackMenuItem = document.getElementById(feedbackId);
@@ -470,17 +481,25 @@ function whenPageLoaded(fn) {
 							!!feedbackButton && feedbackButton.classList.remove('desktop-feedback')
 							const feedbackSvg = document.querySelector(`#${feedbackId} svg`);
 							!!feedbackSvg && feedbackSvg.classList.add(feedbackId);
+
+							const accountButtonOptions = {
+								...accountLinkOptions,
+								id: 'mobile-menu-logged-out-account',
+							};
+							rewriteProvidedPrimoButton(accountButtonOptions, 'md-dialog-content prm-main-menu prm-library-card-menu');
+							rewriteProvidedPrimoButton(searchHistoryOptions, 'md-dialog-content prm-main-menu .my-search-history-ctm');
 						}
 
 						removeElementWhenItAppears('.settings-container prm-authentication'); // "Log in" menu item that duplicates "My account" function
-					}, 250);
+					// }, 5000);
+					}, 100);
 				} else if (!!isDesktopMenuOpen) { // is desktop menu
 					clearInterval(awaitLoggedout);
 
 					const waitForDesktopFeedbackLink = setInterval(() => {
 						const feedbackButton = document.getElementById(DESKTOP_LOGGED_OUT_FEEDBACK_ID);
 						if (!feedbackButton) {
-							clearInterval(waitForDesktopFeedbackLink);
+							// clearInterval(waitForDesktopFeedbackLink);
 
 							rewriteProvidedPrimoButton(accountLinkOptions, '.my-library-card-ctm');
 							rewriteProvidedPrimoButton(favouriteLinkOptions, '.my-favorties-ctm');
@@ -499,7 +518,7 @@ function whenPageLoaded(fn) {
 						}
 					}, 100);
 				}
-			}, 250);
+			}, 100);
 		},
 	});
 
