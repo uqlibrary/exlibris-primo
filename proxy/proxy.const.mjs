d16e8b39114b325262039c7989cd2ec51cc1b653
Lea de Groot <l.degroot@library.uq.edu.au>
2026-03-27 15:31:39 +1000

    working logged in state [YT AD-750]

--- proxy/proxy.const.mjs
+++ proxy/proxy.const.mjs
@@ -1,7 +1,7 @@
 //set the url of the server you want to test your code with and start the development server using the following command:
 // ng serve --proxy-config ./proxy/proxy.conf.mjs
 const environments = {
-    'NDEProd': 'https://uq.primo.exlibrisgroup.com',
+    'NDEProd': 'https://uq-psb.primo.exlibrisgroup.com',
   }
 
   export const PROXY_TARGET = environments['NDEProd'];
--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,7 +1,11 @@
 import {NdeReportAProblemCustomComponent} from "../nde-report-a-problem-custom/nde-report-a-problem-custom.component";
+import {
+  NdeUpdateAccountMenuCustomComponent
+} from "../nde-update-account-menu-custom/nde-update-account-menu-custom.component";
 
 export const selectorComponentMap = new Map<string, any>([
 
   ['nde-full-display-service-container-after', NdeReportAProblemCustomComponent],
+  ['nde-user-area-after', NdeUpdateAccountMenuCustomComponent],
 
 ]);
