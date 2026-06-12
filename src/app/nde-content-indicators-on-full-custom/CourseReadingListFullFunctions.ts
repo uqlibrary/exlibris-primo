bf8aa947a015af480ce0bf28a0877ac06f2109c1
Lea de Groot <l.degroot@library.uq.edu.au>
2026-06-12 11:03:06 +1000

    put CRL in sidebar [YT AD-1000]

--- src/app/nde-content-indicators-on-full-custom/CourseReadingListFullFunctions.ts
+++ src/app/nde-content-indicators-on-full-custom/CourseReadingListFullFunctions.ts
@@ -9,6 +9,33 @@ interface TalisCourse {
     displayName: string;
 }
 
+const mouseoverTooltip = (button: HTMLElement, mouseOverlabel: string) => {
+    const rect = button?.getBoundingClientRect();
+    const pix = 15 * mouseOverlabel.length;
+
+    const tooltipLeft = rect?.left ? rect?.left - (pix / 4) : pix / 4;
+    const tooltipTop = (rect?.top ?? 0) + 32;
+
+    const toolTipHtml = `<uql-tooltip class="cdk-overlay-connected-position-bounding-box" dir="ltr" style="top: 0px; left: 0px; height: 100%; width: 100%;">
+    <div id="cdk-overlay-1" class="cdk-overlay-pane mat-mdc-tooltip-panel-below mat-mdc-tooltip-panel" style="top: ` + tooltipTop + `px; left: ` + tooltipLeft + `px; transform: translateY(8px);">
+        <mat-tooltip-component aria-hidden="true" class="ng-star-inserted">
+            <div class="mdc-tooltip mat-mdc-tooltip mat-mdc-tooltip-show" style="transform-origin: center top;">
+                <div class="mat-mdc-tooltip-surface mdc-tooltip__surface">` + mouseOverlabel + `</div>
+            </div>
+        </mat-tooltip-component>
+    </div>
+</uql-tooltip>`;
+    const template = document.createElement('template');
+    !!template && (template.innerHTML = toolTipHtml);
+    const parent = document.querySelector('.cdk-overlay-container');
+    !!template && parent?.appendChild(template.content.cloneNode(true));
+}
+
+const mouseoutTooltip = () => {
+    const tooltip = document.querySelector('uql-tooltip');
+    !!tooltip && tooltip.remove();
+}
+
 export class CourseReadingListFullFunctions {
     private store = inject(Store);
     searchState = this.store.selectSignal(selectSearchState);
@@ -124,112 +151,124 @@ export class CourseReadingListFullFunctions {
         //     return;
         // }
 
-        const targetElement = document.querySelector('nde-full-display-container mat-accordion');
+        const linkOutIcon: string =
+            '<mat-icon style="height: 20px; width: 18px;" role="img" color="primary" class="mat-icon notranslate nde-mat-icon-size-default primary-stroke mat-primary ng-star-inserted" aria-hidden="true" data-mat-icon-type="svg" data-mat-icon-name="GES">' +
+            '<svg width="16" height="16" viewBox="0 0 24 24">' +
+            '<path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"></path>' +
+            '</svg>' +
+            '</mat-icon>';
 
-        // we use this approach rather than "put this in the angular .html" because we need this block _in the flow_ for layout reasons, not stuck at the bottom
-        let htmlContent = '' +
-            `<div id="nui.brief.results.tabs.crl" _ngcontent-ng-crl="" tabindex="-1" class="ng-star-inserted crl-list-area">
-    <nde-full-display-service-container _ngcontent-ng-crl="" _nghost-ng-crl="" class="ng-star-inserted">
-        <div _ngcontent-ng-crl="">
-            <h2 _ngcontent-ng-crl="" class="visually-hidden">Course reading lists</h2>
-            <mat-expansion-panel id="uql-mat-expansion-panel" _ngcontent-ng-crl="" hidetoggle="" class="mat-expansion-panel mat-expanded mat-expansion-panel-animations-enabled">
-                <mat-expansion-panel-header id="uql-mat-expansion-panel-header-button" _ngcontent-ng-crl="" role="button" class="mat-expansion-panel-header mat-focus-indicator mat-expanded mat-expansion-toggle-indicator-after" aria-labelledby="title-nui.brief.results.tabs.crl" tabindex="0" aria-controls="cdk-accordion-child-4" aria-expanded="true" aria-disabled="false">
-                    <span class="mat-content mat-content-hide-toggle">
-                        <mat-panel-title _ngcontent-ng-crl="" class="mat-expansion-panel-header-title">
-                            <span id="crl-label" data-testid="course-reading-list" _ngcontent-ng-crl="" class="title-first-upper mat-title-medium" >Course reading lists</span>
-                        </mat-panel-title>
-                        <mat-panel-description _ngcontent-ng-crl="" class="mat-expansion-panel-header-description">
-                            <mat-icon id="uql-hide-crl-area" _ngcontent-ng-crl="" role="img" color="primary" class="uql-hide-crl-area mat-icon notranslate nde-mat-icon-size-default primary-stroke mat-primary ng-star-inserted" aria-hidden="true" data-mat-icon-type="svg" data-mat-icon-name="Remove">
-                                <svg width="100%" height="100%" viewBox="0 0 16 2" fill="none" fit="" preserveAspectRatio="xMidYMid meet" focusable="false">
-                                    <path d="M0 1H16" stroke-width="2"></path>
-                                </svg>
-                            </mat-icon>
-                            <mat-icon id="uql-show-crl-area" style="display: none" role="img" color="primary" class="uql-show-crl-area mat-icon notranslate nde-mat-icon-size-default primary-stroke line-height-normal mat-primary ng-star-inserted" aria-hidden="true" data-mat-icon-type="svg" data-mat-icon-name="Add">
-                                <svg width="100%" height="100%" viewBox="0 0 16 16" fit="" preserveAspectRatio="xMidYMid meet" focusable="false">
-                                    <path d="M0 8H16" stroke-width="2"></path>
-                                    <path d="M8 0L8 16" stroke-width="2"></path>
-                                </svg>
-                            </mat-icon>   
-                        </mat-panel-description>
-                    </span>
-                </mat-expansion-panel-header>
-                <div class="mat-expansion-panel-content-wrapper">
-                    <div id="uql-accordion-child-crl" role="region" class="uql-accordion-child-crl mat-expansion-panel-content" aria-labelledby="mat-expansion-panel-header-3">
-                        <div class="mat-expansion-panel-body">
-                            <nde-full-display-crl _ngcontent-ng-crl="" _nghost-ng-crl="" class="ng-star-inserted">
-                                <div _ngcontent-ng-crl="" data-qa="full_display_crl_online_crl" class="mat-title-small crl-container flex-column gap-05 ng-star-inserted">
+        const showAllButtonLabel = 'Show all';
+        const showLessButtonLabel = 'Show less';
+        const maxNumberReadingListsDisplayed = 8; // when there are a lot of course, we show a 'show all' button when there are more than this number
+        const crlHiddenClass = 'uql-crl-list-hidden'; // courses which are actually hidden, add/remove this classname
+        const crlHideableClass = `uql-crl-list-hideable`; // courses which are > maxNumberReadingListsDisplayed, find this to add/remove crlHiddenClass
+
+        const targetElement = document.querySelector('nde-full-display-side-bar');
+
+        let htmlContent = `<nde-full-display-crl _nghost-ng-crl="" class="ng-star-inserted">
+            <nde-collapsible-box _ngcontent-ng-crl="" class="course-reading-list-container" _nghost-ng-crl="">
+                <mat-expansion-panel _ngcontent-ng-crl="" class="mat-expansion-panel mat-elevation-z0 mat-expanded mat-expansion-panel-animations-enabled">
+                    <mat-expansion-panel-header _ngcontent-ng-crl="" role="button"
+                            class="mat-expansion-panel-header mat-focus-indicator with-tooltip-anchor mat-expanded"
+                            aria-labelledby="title.Course Reading Lists" id="mat-expansion-panel-header-crl"
+                            tabindex="0" aria-controls="cdk-accordion-child-crl" aria-expanded="true"
+                            aria-disabled="false">
+                        <span class="mat-content">
+                            <h2 _ngcontent-ng-crl="" id="title.Course Reading Lists">Course Reading Lists</h2>
+                            <span _ngcontent-ng-crl="" mattooltipposition="below" aria-hidden="true"
+                                class="mat-mdc-tooltip-trigger tooltip-anchor" aria-describedby="cdk-describedby-message-ng-crl"
+                                cdk-describedby-host="ng-1">
+                            </span>
+                        </span>
+                        <span id="uql-mat-expansion-panel-header-button" class="mat-expansion-indicator ng-star-inserted">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" aria-hidden="true" focusable="false"><path d="M480-345 240-585l56-56 184 184 184-184 56 56-240 240Z"></path></svg>
+                        </span>
+                    </mat-expansion-panel-header>
+                    <div class="mat-expansion-panel-content-wrapper">
+                        <div role="region" id="uql-accordion-child-crl" class="mat-expansion-panel-content" id="cdk-accordion-child-crl" aria-labelledby=mat-expansion-panel-header-crl">
+                            <div class="mat-expansion-panel-body">
+                                <p _ngcontent-ng-crl="" id="search-within-desc" class="mat-body-medium">This resource is on the Reading list for these subjects:</p>
                                 <ul class="course-resource-list">`;
-        for (const [url, displayName] of Object.entries(talisCourses)) {
-            htmlContent += '<li>' +
-                `<a class="uql-crl-list-item" href="${url}" target="_blank">` + //  button-as-link link-alt-color md-button md-primoExplore-theme md-ink-ripple
-                `<span>${displayName}</span>` +
-                '<mat-icon style="height: 20px; width: 18px;" role="img" color="primary" class="mat-icon notranslate nde-mat-icon-size-default primary-stroke mat-primary ng-star-inserted" aria-hidden="true" data-mat-icon-type="svg" data-mat-icon-name="GES">' +
-                '<svg width="16" height="16" viewBox="0 0 24 24">' +
-                '<path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"></path>' +
-                '</svg>' +
-                '</mat-icon>' +
-                '</a>' +
-                '</li>';
+        let numberOfReadingLists = 0;
+        for (const [url, displayName] of Object.entries(talisCourses) as [string, string][]) {
+            const className = numberOfReadingLists < maxNumberReadingListsDisplayed ? 'uql-crl-list-constant': `${crlHideableClass} ${crlHiddenClass}`;
+            htmlContent += `<li class="uql-crl-list ${className}">
+                <a class="uql-crl-list-item" href="${url}" target="_blank">
+                    <span>${displayName}</span>
+                    ${linkOutIcon}
+                </a></li>`;
+            numberOfReadingLists++;
         }
-        htmlContent += '' +
-            `</ul>
-                              </div>
-                            </nde-full-display-crl>
-                        </div>
-                    </div>
+        htmlContent += `</ul>`;
+        if (numberOfReadingLists >= maxNumberReadingListsDisplayed) {
+            htmlContent += `<div class="toggle-show-all-button"><button id="toggle-long-crl" class="collapsed"><span>${showAllButtonLabel}</span><span style="display: none">Show less</span></button></div>`;
+        }
+        htmlContent += `</div>
                 </div>
-            </mat-expansion-panel>
-        </div>
-    </nde-full-display-service-container>`;
-
-        // Create a temporary container to attach the HTML
-        const tempContainer = document.createElement('div');
-        !!tempContainer && (tempContainer.id = "full-view-section-courseReadingLists");
-        !!tempContainer && tempContainer.classList.add('full-view-section', 'readingListCitations');
-        !!tempContainer && (tempContainer.tabIndex = -1);
-        tempContainer.innerHTML = htmlContent;
+            </div>
+        </mat-expansion-panel>
+    </nde-collapsible-box>
+</nde-full-display-crl>`;
+        const template = document.createElement('template');
+        template.innerHTML = htmlContent;
 
         // Insert the course list as the first child of the target element
-        !!targetElement && targetElement.appendChild(tempContainer);
+        !!targetElement && targetElement.prepend(template.content.cloneNode(true));
 
-        const CRLToggleButton = document.getElementById('uql-mat-expansion-panel-header-button');
-        !!CRLToggleButton && CRLToggleButton.addEventListener('click', function (event) {
+        // handle the show-all show-less click when there are many courses
+        const longToggleButton = document.getElementById('toggle-long-crl');
+        !!longToggleButton && longToggleButton.addEventListener('click', function (event) {
+            const hiddenCRL = document.querySelectorAll(`.${crlHiddenClass}`);
+            if (hiddenCRL?.length > 0) {
+                // hiding the entries - show them
+                hiddenCRL.forEach(c => c.classList.remove(crlHiddenClass))
+                longToggleButton.innerHTML = showLessButtonLabel;
+            } else {
+                // visible entries - hide them
+                const hideableCRL = document.querySelectorAll(`.${crlHideableClass}`);
+                hideableCRL?.forEach(c =>  c.classList.add(crlHiddenClass))
+                longToggleButton.innerHTML = showAllButtonLabel
+                // scroll the top into view, rather than leaving it floating in the midle of the page
+                document.getElementById('mat-expansion-panel-header-crl')?.scrollIntoView();
+            }
+        });
+
+        // handle the collapse-expand of the panel, mimicking the build-in
+        let mouseOverPrefix = 'Collapse';
+        const panelToggleButton = document.getElementById('uql-mat-expansion-panel-header-button');
+        !!panelToggleButton && panelToggleButton.addEventListener('click', function (event) {
             event.preventDefault();
 
+            const panel = document.querySelector('nde-full-display-crl');
+
             const listArea = document.getElementById('uql-accordion-child-crl');
-            !!listArea && (listArea.style.visibility = listArea.style.visibility === 'hidden' ? 'visible' : 'hidden');
 
-            const crlAreaButton_hideIcon = document.getElementById('uql-hide-crl-area');
-            if (!!crlAreaButton_hideIcon) {
-                crlAreaButton_hideIcon.style.display = crlAreaButton_hideIcon.style.display === 'none' ? 'block' : 'none';
+            const panelHeader = panel?.querySelector('mat-expansion-panel-header');
+            panelHeader?.classList.toggle('mat-expanded');
+
+            if (!!listArea) {
+                console.log('crl## visibility=', !!listArea && listArea.style.visibility);
+                console.log('crl## height=', !!listArea && listArea.style.height);
+                panelHeader?.setAttribute(
+                    'aria-expanded',
+                    listArea.style.visibility === 'hidden' ? 'true' : 'false'
+                )
+                mouseOverPrefix = !!listArea && listArea.style.visibility === 'hidden' ? 'Collapse' : 'Expand';
+                listArea.style.visibility = listArea.style.visibility === 'hidden' ? 'visible' : 'hidden';
+
+                listArea.style.unicodeBidi = listArea.style.height === '0px' ? '' : 'isolate';
+                listArea.style.height = listArea.style.height === '0px' ? '' : '0px';
             }
+        });
+        const listArea = document.getElementById('uql-accordion-child-crl');
+        !!panelToggleButton && panelToggleButton.addEventListener('mouseover', function (event) {
+            const mouseOverLabel = `${mouseOverPrefix} Course reading lists`;
+            mouseoverTooltip(panelToggleButton, mouseOverLabel);
+        });
 
-            const crlAreaButton_showIcon = document.getElementById('uql-show-crl-area');
-            const crlButton = document.getElementById('uql-mat-expansion-panel-header-button');
-            const matExpansionPanel = document.getElementById('uql-mat-expansion-panel');
-            const matExpansionPanelContentWrapper = document.getElementById('mat-expansion-panel-content-wrapper');
-            if (!!crlAreaButton_showIcon) {
-                if (crlAreaButton_showIcon.style.display === 'none') {
-                    crlAreaButton_showIcon.style.display = 'block'
-                    // !!crlButton && !crlButton.classList.contains('uqlHoverGrey') && crlButton.classList.add('uqlHoverGrey');
-                    !!matExpansionPanel && matExpansionPanel.classList.contains('mat-expanded') && matExpansionPanel.classList.remove('mat-expanded');
-                    !!crlButton && crlButton.classList.contains('mat-expanded') && crlButton.classList.remove('mat-expanded');
-                    !!matExpansionPanelContentWrapper && matExpansionPanelContentWrapper.classList.contains('mat-expanded') && matExpansionPanelContentWrapper.classList.remove('mat-expanded');
-
-
-                    !!crlButton && crlButton.setAttribute('aria-expanded', 'false');
-                } else {
-                    crlAreaButton_showIcon.style.display = 'none';
-                    // !!crlButton && crlButton.classList.contains('uqlHoverGrey') && crlButton.classList.remove('uqlHoverGrey');
-                    !!matExpansionPanel && !matExpansionPanel.classList.contains('mat-expanded') && matExpansionPanel.classList.add('mat-expanded');
-                    !!crlButton && !crlButton.classList.contains('mat-expanded') && crlButton.classList.add('mat-expanded');
-                    !!matExpansionPanelContentWrapper && !matExpansionPanelContentWrapper.classList.contains('mat-expanded') && matExpansionPanelContentWrapper.classList.add('mat-expanded');
-
-                    !!crlButton && crlButton.setAttribute('aria-expanded', 'true');
-                }
-            }
-
-            return false;
+        !!panelToggleButton && panelToggleButton.addEventListener('mouseout', function (event) {
+            mouseoutTooltip();
         });
     }
 
