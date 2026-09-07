badd4a60e75eeab563e7c47026ca47e2f0b2452e
Lea de Groot <l.degroot@library.uq.edu.au>
2026-09-08 09:39:56 +1000

    refactor [doc #56] [YT AD-1243]

--- src/app/nde-hero-block-custom/nde-hero-block-custom.component.ts
+++ src/app/nde-hero-block-custom/nde-hero-block-custom.component.ts
@@ -12,57 +12,68 @@ export class NdeHeroBlockCustomComponent {
     private elementRef = inject(ElementRef);
 
     ngOnInit(): void {
+        const hostElement = findHostElement(this.elementRef.nativeElement);
         if (window.location.pathname.startsWith('/nde/collectionDiscovery')) {
-            clearExistingHero();
+            clearExistingHero(); // so can go from citation finder to colldisc and lose citation finder header
         } else if (window.location.pathname.startsWith('/nde/citationlinker')) {
-            const hostElement = findHostElement(this.elementRef.nativeElement);
-            const displayedTitleElement = hostElement?.querySelector('h1');
-            const heroLabel = displayedTitleElement?.textContent;
+            this.writeCitationFinderHero(hostElement);
+        } else if (window.location.pathname.startsWith('/nde/dbsearch')) {
+            this.writeDbSearchHero(hostElement);
+        } else if (window.location.pathname.startsWith('/nde/purchaseRequest')) {
+            this.writeFormHero(hostElement);
+        } else if (window.location.pathname.startsWith('/nde/blankIll')) { // resource delivery request form
+            this.writeFormHero(hostElement);
+        }
+    }
+
+    private writeCitationFinderHero(hostElement: Element | null){
+        const displayedTitleElement = hostElement?.querySelector('h1');
+        const heroLabel = displayedTitleElement?.textContent;
+        if (!displayedTitleElement) {
+            return;
+        }
+
+        // remove the displayed title so we can replace it with a hero banner
+        displayedTitleElement?.remove();
+
+        const newHeroElement = getHeroElement(heroLabel, 'citationfinder');
+
+        !!newHeroElement && hostElement?.parentNode?.insertBefore(newHeroElement, hostElement);
+    }
+
+    private writeDbSearchHero(hostElement: Element | null){
+        const params = new URLSearchParams(window.location.search);
+        if (params.has("query")) {
+            // only the dbsearch homepage gets a big hero header
+            return;
+        }
+
+        addClassName(hostElement, 'uq-hero');
+
+        const h1 = hostElement?.querySelector('h1');
+        addClassName(h1, 'uq-hero__content');
+    }
+
+    private writeFormHero(hostElement: Element | null){
+        const awaitLoad = setInterval(() => {
+            const displayedTitleElement = hostElement?.querySelector('h2.request-title');
             if (!displayedTitleElement) {
                 return;
             }
+            clearInterval(awaitLoad);
+
+            const heroLabel = displayedTitleElement?.textContent;
 
             // remove the displayed title so we can replace it with a hero banner
             displayedTitleElement?.remove();
 
-            const newHeroElement = getHeroElement(heroLabel, 'citationfinder');
+            const uniqueStringForPageType = window.location.pathname.replace(/\//g, '');
+            const newHeroElement = getHeroElement(heroLabel, uniqueStringForPageType);
 
-            !!newHeroElement && hostElement?.parentNode?.insertBefore(newHeroElement, hostElement);
-        } else if (window.location.pathname.startsWith('/nde/dbsearch')) {
-            const hostElement = findHostElement(this.elementRef.nativeElement);
+            const h1 = document.querySelector('h1');
+            !!newHeroElement && h1?.parentNode?.replaceChild(newHeroElement, h1);
 
-            const params = new URLSearchParams(window.location.search);
-            if (!params.has("query")) {
-                // only the dbsearch homepage gets a big hero header
-                addClassName(hostElement, 'uq-hero');
-
-                const h1 = hostElement?.querySelector('h1');
-                addClassName(h1, 'uq-hero__content');
-            }
-        } else if (window.location.pathname.startsWith('/nde/purchaseRequest') || window.location.pathname.startsWith('/nde/blankIll')) {
-            const awaitLoad = setInterval(() => {
-                let hostElement = findHostElement(this.elementRef.nativeElement);
-                if (!hostElement) {
-                    hostElement = findHostElement(this.elementRef.nativeElement);
-                }
-                const displayedTitleElement = hostElement?.querySelector('h2.request-title');
-                const heroLabel = displayedTitleElement?.textContent;
-                if (!displayedTitleElement) {
-                    return;
-                }
-
-                clearInterval(awaitLoad);
-
-                // remove the displayed title so we can replace it with a hero banner
-                displayedTitleElement?.remove();
-
-                const uniqueStringForPageType = window.location.pathname.replace(/\//g, '');
-                const newHeroElement = getHeroElement(heroLabel, uniqueStringForPageType);
-
-                const h1 = document.querySelector('h1');
-                !!newHeroElement && h1?.parentNode?.replaceChild(newHeroElement, h1);
-
-            }, 1000);
-        }
+        }, 1000);
     }
+
 }
